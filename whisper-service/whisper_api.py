ef556bd70c4e85c0513a4054299966ac1c576f44
GaileAlex <aleksei.gaile@gmail.com>
2025-11-10 20:19:46 +0200

    Whisper service

--- whisper-service/whisper_api.py
+++ whisper-service/whisper_api.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import tempfile
 import os
+import torch
 import whisper
 
 app = Flask(__name__)
 
-model = whisper.load_model("large", device="cuda")
+model = whisper.load_model("medium", device="cpu")
 
 @app.route("/transcribe", methods=["POST"])
 def transcribe():
@@ -20,17 +21,24 @@ def transcribe():
         tmp_path = tmp.name
 
     try:
-        if language:
-            result = model.transcribe(tmp_path, language=language)
-        else:
-            result = model.transcribe(tmp_path)  # автоопределение языка
+        model.to("cuda")
 
-        return jsonify({
-            "text": result["text"],
-            "language": result.get("language")
-        })
+        result = model.transcribe(tmp_path, language=language)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
     finally:
         os.remove(tmp_path)
+
+        model.to("cpu")
+        torch.cuda.empty_cache()
+
+    return jsonify({
+        "text": result["text"],
+        "language": result.get("language")
+    })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
